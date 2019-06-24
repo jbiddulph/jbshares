8e57a710c34992a23a2a99e2ddab53b83fa54b79
John Biddulph <john.mbiddulph@gmail.com>
2019-06-24 21:38:13 +0100

    first commit

---
+++ classes/bootstrap.php
@@ -0,0 +1,46 @@
+<?php
+class Bootstrap {
+    private $controller;
+    private $action;
+    private $request;
+
+    public function __construct($request) {
+        $this->request = $request;
+        if($this->request['controller'] == ''){
+            $this->controller = 'home';
+        } else {
+            $this->controller = $this->request['controller'];
+        }
+
+        if($this->request['action'] == ''){
+            $this->action = 'index';
+        } else {
+            $this->action = $this->request['action'];
+        }
+    }
+
+    public function createController(){
+        //Check Class
+        if(class_exists($this->controller)){
+            $parents = class_parents($this>$this->controller);
+            //check extend
+            if(in_array("Controller", $parents)){
+                if(method_exists($this->controller, $this->action)){
+                    return new $this->controller($this->action, $this->request);
+                } else {
+                    //Method does not exist
+                    echo '<h1>Method does not exist</h1>';
+                    return;
+                }
+            } else {
+                //Base Controller not found
+                echo '<h1>Base controller not found</h1>';
+                return;
+            }
+        } else {
+            //Controller Class does not exist
+            echo '<h1>Controller class does not exist</h1>';
+            return;
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//Include Config
+require('config.php');
+
+require('classes/bootstrap.php');
+
+$bootstrap = new Bootstrap($_GET);
+
+$controller = $bootstrap->createController();
